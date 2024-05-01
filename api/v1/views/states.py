2dd38a024e638eb37376c11f3adf02b8cf4b9ecf
sara90-boubou <saraboubou4@gmail.com>
2024-05-01 04:38:29 -0700

    updated

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,112 +1,72 @@
 #!/usr/bin/python3
-""" Flask routes for `State` object related URI subpaths using the `app_views`
-Blueprint.
-"""
+"""states"""
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
+from flask import jsonify, abort, request
 from models import storage
 from models.state import State
+from datetime import datetime
+import uuid
 
 
-@app_views.route("/states", methods=['GET'],
-                 strict_slashes=False)
-def GET_all_State():
-    """ Returns JSON list of all `State` instances in storage
-
-    Return:
-        JSON list of all `State` instances
-    """
-    state_list = []
-    for state in storage.all(State).values():
-        state_list.append(state.to_dict())
-
-    return jsonify(state_list)
+@app_views.route('/states/', methods=['GET'])
+def list_states():
+    '''Retrieves a list of all State objects'''
+    list_states = [obj.to_dict() for obj in storage.all("State").values()]
+    return jsonify(list_states)
 
 
-@app_views.route("/states/<state_id>", methods=['GET'],
-                 strict_slashes=False)
-def GET_State(state_id):
-    """ Returns `State` instance in storage by id in URI subpath
-
-    Args:
-        state_id: uuid of `State` instance in storage
-
-    Return:
-        `State` instance with corresponding uuid, or 404 response
-    on error
-    """
-    state = storage.get(State, state_id)
-
-    if state:
-        return jsonify(state.to_dict())
-    else:
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    '''Retrieves a State object'''
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
         abort(404)
+    return jsonify(state_obj[0])
 
 
-@app_views.route("/states/<state_id>", methods=['DELETE'],
-                 strict_slashes=False)
-def DELETE_State(state_id):
-    """ Deletes `State` instance in storage by id in URI subpath
-
-    Args:
-        state_id: uuid of `State` instance in storage
-
-    Return:
-        Empty dictionary and response status 200, or 404 response
-    on error
-    """
-    state = storage.get(State, state_id)
-
-    if state:
-        storage.delete(state)
-        storage.save()
-        return ({})
-    else:
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    '''Deletes a State object'''
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
         abort(404)
+    state_obj.remove(state_obj[0])
+    for obj in all_states:
+        if obj.id == state_id:
+            storage.delete(obj)
+            storage.save()
+    return jsonify({}), 200
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
-def POST_State():
-    """ Creates new `State` instance in storage
-
-    Return:
-        Empty dictionary and response status 200, or 404 response
-    on error
-    """
-    req_dict = request.get_json()
-    if not req_dict:
-        return (jsonify({'error': 'Not a JSON'}), 400)
-    elif 'name' not in req_dict:
-        return (jsonify({'error': 'Missing name'}), 400)
-    new_State = State(**req_dict)
-    new_State.save()
-
-    return (jsonify(new_State.to_dict()), 201)
+@app_views.route('/states/', methods=['POST'])
+def create_state():
+    '''Creates a State'''
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+    states = []
+    new_state = State(name=request.json['name'])
+    storage.new(new_state)
+    storage.save()
+    states.append(new_state.to_dict())
+    return jsonify(states[0]), 201
 
 
-@app_views.route("/states/<state_id>", methods=['PUT'],
-                 strict_slashes=False)
-def PUT_State(state_id):
-    """ Updates `State` instance in storage by id in URI subpath, with
-    kwargs from HTTP body request JSON dict
-
-    Args:
-        state_id: uuid of `State` instance in storage
-
-    Return:
-        Empty dictionary and response status 200, or 404 response
-    on error
-    """
-    state = storage.get(State, state_id)
-    req_dict = request.get_json()
-
-    if state:
-        if not req_dict:
-            return (jsonify({'error': 'Not a JSON'}), 400)
-        for key, value in req_dict.items():
-            if key not in ['id', 'created_at', 'updated_at']:
-                setattr(state, key, value)
-        storage.save()
-        return (jsonify(state.to_dict()))
-    else:
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def updates_state(state_id):
+    '''Updates a State object'''
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
         abort(404)
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    state_obj[0]['name'] = request.json['name']
+    for obj in all_states:
+        if obj.id == state_id:
+            obj.name = request.json['name']
+    storage.save()
+    return jsonify(state_obj[0]), 200
